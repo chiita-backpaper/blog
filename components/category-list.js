2867c870ef2e8e64e2c67ece193bf560ad563020
Taichi <tu.0514.0315@icloud.com>
2021-09-22 16:52:46 +0900

    classNameでクラスを指定

--- components/category-list.js
+++ components/category-list.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 export default function CategoryList({ categories }) {
     return (
-        <div class="hidden xl:text-sm xl:block flex-none w-64 pl-8 mr-8">
-            <div class="flex flex-col justify-between overflow-y-auto sticky max-h-(screen-18) pt-10 pb-6 top-18">
-                <div class="mb-8"></div>
-                <ul class="overflow-x-hidden text-gray-500 font-medium">
+        <div className="hidden xl:text-sm xl:block flex-none w-64 pl-8 mr-8">
+            <div className="flex flex-col justify-between overflow-y-auto sticky max-h-(screen-18) pt-10 pb-6 top-18">
+                <div className="mb-8"></div>
+                <ul className="overflow-x-hidden text-gray-500 font-medium">
                     {categories.map((category) => {
                         return (
-                            <li class="block transform transition-colors duration-200 py-2 hover:text-gray-900 text-gray-900" key={category}>
+                            <li className="block transform transition-colors duration-200 py-2 hover:text-gray-900 text-gray-900" key={category}>
                                 <Link as={`/categories/${category}`} href="/categories/[category]">
                                     <a className="hover:underline"> {category}</a>
                                 </Link>
