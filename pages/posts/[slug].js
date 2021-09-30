ed945f8e0bfdeb53133d4510cb8deea867870343
Taichi Uragami <tu.0514.0315@icloud.com>
2021-10-01 00:23:07 +0900

    Add a category list to detail and initial page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,17 +1,18 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import { Container } from '../../components/container'
+import { Container, FlexContainer, CategoryContainer } from '../../components/container'
 import PostBody from '../../components/post-body'
 import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
+import CategoryList from '../../components/category-list'
 import Layout from '../../components/layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
+import { getAllPosts, getPostsByCategory, getPostSlugs, getPostBySlug, getAllCategories } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 
-export default function Post({ post, morePosts, preview }) {
+export default function Post({ post, categories, morePosts, preview }) {
   const router = useRouter()
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
@@ -20,28 +21,31 @@ export default function Post({ post, morePosts, preview }) {
     <Layout preview={preview}>
       <Container>
         <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {"yomoyama-" + post.title}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                category={post.category}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
+        <FlexContainer>
+          {router.isFallback ? (
+            <PostTitle>Loading…</PostTitle>
+          ) : (
+            <>
+              <article className="mb-32">
+                <Head>
+                  <title>
+                    {"yomoyama-" + post.title}
+                  </title>
+                  <meta property="og:image" content={post.ogImage.url} />
+                </Head>
+                <PostHeader
+                  title={post.title}
+                  category={post.category}
+                  coverImage={post.coverImage}
+                  date={post.date}
+                  author={post.author}
+                />
+                <PostBody content={post.content} />
+              </article>
+            </>
+          )}
+          <CategoryList categories={categories} />
+        </FlexContainer>
       </Container>
     </Layout>
   )
@@ -59,6 +63,7 @@ export async function getStaticProps({ params }) {
     'coverImage',
   ])
   const content = await markdownToHtml(post.content || '')
+  const categories = getAllCategories()
 
   return {
     props: {
@@ -66,6 +71,7 @@ export async function getStaticProps({ params }) {
         ...post,
         content,
       },
+      categories: categories
     },
   }
 }
